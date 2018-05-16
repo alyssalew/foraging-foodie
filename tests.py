4c72dc151446c8826167eb7142bb680063744af5
Alyssa <alyssalew2@gmail.com>
2018-05-16 03:19:16 +0000

    Fix root route test

--- tests.py
+++ tests.py
@@ -1,8 +1,8 @@
 
-from unittest import TestCase
+import unittest
 from server import app
 
-class FlaskTests(TestCase):
+class FlaskTests(unittest.TestCase):
     def setUp(self):
         """Stuff to do before every test."""
         self.client = app.test_client()
@@ -11,13 +11,17 @@ class FlaskTests(TestCase):
     def tearDown(self):
         """Stuff to do after each test."""
 
-    def test1(self):
+    def test_root(self):
         """Testing the root route"""
 
-        client = app.test_client()
-        result = client.get('/')
-        self.assertIn('<h1> Welcome to The Foraging Foodie </h1>', result)
+        result = self.client.get('/')
+        # print result.data
+        self.assertIn('<h1> Welcome to The Foraging Foodie </h1>', result.data)
 
 
-    def test2(self):
-        """Some other test"""
+    # def test2(self):
+    #     """Some other test"""
+
+
+if __name__ == "__main__":
+    unittest.main()
