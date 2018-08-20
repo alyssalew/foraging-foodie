fc68d606655bdb6de4bddd26f75b81333c03da92
Alyssa <alyssalew2@gmail.com>
2018-08-20 00:32:43 +0000

    Update tests for route changes

--- tests.py
+++ tests.py
@@ -23,8 +23,10 @@ class FlaskTests(unittest.TestCase):
 
     def test_render_root(self):
         """Testing the Root route"""
+        
+        print "test_render_root"
 
-        result = self.client.get('/')
+        result = self.client.get('/foraging-foodie')
         # print result.data
         self.assertEqual(result.status_code, 200)
         self.assertIn('<h1 id="welcome"> Welcome to The Foraging Foodie </h1>', result.data)
@@ -32,7 +34,9 @@ class FlaskTests(unittest.TestCase):
     def test_render_register(self):
         """Testing the Register route"""
 
-        result = self.client.get('/register')
+        print "test_render_register"
+
+        result = self.client.get('/foraging-foodie/register')
         # print result.data
         self.assertEqual(result.status_code, 200)
         self.assertIn('<h3>Register</h3>', result.data)
@@ -41,7 +45,9 @@ class FlaskTests(unittest.TestCase):
     def test_render_login(self):
         """Testing the Login route"""
 
-        result = self.client.get('/login')
+        print "test_render_login"
+
+        result = self.client.get('/foraging-foodie/login')
         # print result.data
         self.assertEqual(result.status_code, 200)
         self.assertIn('<h3>Login</h3>', result.data)
@@ -49,19 +55,21 @@ class FlaskTests(unittest.TestCase):
     # def test_login(self):
     #     """Test login page."""
 
-    #     result = self.client.post("/verify-login",
+    #     print "test_login"
+
+    #     result = self.client.post("/foraging-foodie/verify-login",
     #                               data={"email": "alyssa@example.com", "password": "123"},
     #                               follow_redirects=True)
     #     self.assertIn('<ul class="flashes">Hi Alyssa, you are now logged in!</ul>', result.data)
 
 
-    # To send real request to Yelp API, comment out mock ^^
+    # To send real request to Yelp API, comment out '_mock_request_restaurants' ^^
     def test_search(self):
         """Testing the restaurant search"""
 
-        print "This is a mock search."
+        print "test_search"
 
-        result = self.client.post('/results',
+        result = self.client.post('/foraging-foodie/results',
                                 data={"location": "San Francisco", "radius": 5,
                                         "limit": 5, "diet_restrict":['vegan'], "taste":['spicy']})
         # print result.data
