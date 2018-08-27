6afecf5fe567b88b12b598032d04817629608df2
Alyssa <alyssalew2@gmail.com>
2018-08-27 04:02:43 +0000

    Fix autofill for multiple saved addresses

--- static/foraging-foodie/js/insert-my-address.js
+++ static/foraging-foodie/js/insert-my-address.js
@@ -4,16 +4,16 @@ function replaceLocation(results) {
     // console.log("Replace location");
     // $("input[name='inlineRadioOptions']:checked").val();
 
-   $( "#location" ).val( $( "#inlineRadio-my-address:checked" ).val());
+   $( "#location" ).val( $( "#inlineRadio-my-address.form-check-input:checked" ).val());
 }
 
 
 function enterLocation(results){
     // console.log ("Clicked into textbox");
     $( "#location" ).val('');
-    $("#inlineRadio-my-address").prop('checked', false);
+    $("#inlineRadio-my-address.form-check-input").prop('checked', false);
 }
 
-$( "#inlineRadio-my-address" ).on( "click", replaceLocation);
+$( "#inlineRadio-my-address.form-check-input" ).on( "click", replaceLocation);
 
 $( "#location" ).focus(enterLocation);
