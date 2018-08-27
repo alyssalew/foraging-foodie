d5de5bc47735163ccd57ccdfaec5bd9fa5e36b22
Alyssa <alyssalew2@gmail.com>
2018-08-27 03:38:52 +0000

    Fix autofill from checkboxes bug

--- static/foraging-foodie/js/insert-my-address.js
+++ static/foraging-foodie/js/insert-my-address.js
@@ -4,16 +4,16 @@ function replaceLocation(results) {
     // console.log("Replace location");
     // $("input[name='inlineRadioOptions']:checked").val();
 
-   $( "#location" ).val( $( ".form-check-input:checked" ).val());
+   $( "#location" ).val( $( "#inlineRadio-my-address:checked" ).val());
 }
 
 
 function enterLocation(results){
     // console.log ("Clicked into textbox");
     $( "#location" ).val('');
-    $(".form-check-input").prop('checked', false);
+    $("#inlineRadio-my-address").prop('checked', false);
 }
 
-$( ".form-check-input" ).on( "click", replaceLocation);
+$( "#inlineRadio-my-address" ).on( "click", replaceLocation);
 
 $( "#location" ).focus(enterLocation);
